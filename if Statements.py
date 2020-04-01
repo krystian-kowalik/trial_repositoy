9a5899ba231eed80bdb1a2205f671e83b5983a2f
krystian-kowalik <kjkowalik05@gmail.com>
2020-04-01 21:49:07 +0200

    just checking

--- if Statements.py
+++ if Statements.py
@@ -1,7 +1,12 @@
 
-is_boy = True     
+is_boy = True
+is_tall = False
 
-if is_boy:
-    print("You are a boy.")
+if is_boy and is_tall:
+    print("You are a tall boy.")
+elif is_boy and not (is_tall):
+    print("You are a short boy")
+elif not(is_boy) and is_tall:
+    print("You aren't a boy but are tall")
 else:
-    print("You aren't a boy.")
+    print("You are either not boy or not tall or both.")
