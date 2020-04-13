99e0a1d663be3be3872216bc359e313ee328fbeb
krystian-kowalik <kjkowalik05@gmail.com>
2020-04-13 12:38:28 +0200

    just checking

---
+++ dictionary.py
@@ -0,0 +1,14 @@
+wordsConversions = {
+    "ciel": "niebo",
+    "tou": "ty",
+    "herbe": "trawa",
+    "aliénée": "wyalienowany",
+    "idéal": "wymarzony",
+    "castor": "bóbr",
+    "frère": "brat",
+    "serrure": "zamek",
+    "non préparé": "nieprzygotowany",
+    "favori": "ulubiony"
+}
+
+print(wordsConversions["favori"])
